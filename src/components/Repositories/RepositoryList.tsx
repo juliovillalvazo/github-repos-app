8b2d0fdb990cc55b816eb738ef56264bc7560a3d
Julio Villalvazo Carrera <juliovillalvazo26@gmail.com>
2023-03-19 23:29:23 -0600

    feat: modified styles for sorting menu

--- src/components/Repositories/RepositoryList.tsx
+++ src/components/Repositories/RepositoryList.tsx
@@ -1,8 +1,8 @@
-import { Dimensions, FlatList, View, StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import { SingleRepository } from '../../types';
 import useRepositories from '../../hooks/useRepositories';
-import { Menu, Divider, Button } from 'react-native-paper';
+import { Menu, Divider, Button, Modal, Portal } from 'react-native-paper';
 import { useState } from 'react';
 import theme from '../../theme';
 
@@ -40,8 +40,12 @@ const RepositoryList = () => {
     const [orderDirection, setOrderDirection] = useState('DESC');
     const { repositories } = useRepositories(orderBy, orderDirection);
 
-    const openMenu = () => setVisible(true);
-    const closeMenu = () => setVisible(false);
+    const openMenu = () => {
+        setVisible(true);
+    };
+    const closeMenu = () => {
+        setVisible(false);
+    };
 
     const menuLabel = () => {
         switch (orderBy) {
@@ -57,57 +61,168 @@ const RepositoryList = () => {
     return (
         <View style={{ height: '100%' }}>
             <Menu
-                anchor={<Button onPress={openMenu}>{menuLabel()}</Button>}
+                anchor={
+                    <Button
+                        textColor={theme.colors.textSecondary}
+                        icon='menu'
+                        onPress={openMenu}
+                        loading={visible}
+                        mode='elevated'
+                        style={{
+                            borderRadius: 0,
+                            backgroundColor: theme.colors.mainBackgroundColor,
+                        }}
+                    >
+                        {menuLabel()}
+                    </Button>
+                }
                 anchorPosition='bottom'
                 onDismiss={closeMenu}
                 visible={visible}
                 contentStyle={{
                     backgroundColor: theme.colors.primary,
-                    left: Dimensions.get('window').width - 200,
-                    alignItems: 'flex-end',
-                    width: 200,
+                    width: '100%',
                 }}
-                style={{ width: '100%' }}
             >
-                <Menu.Item
-                    onPress={() => {
-                        setOrderBy('CREATED_AT');
-                        setOrderDirection('ASC');
-                        closeMenu();
-                    }}
-                    title='Oldest repositories'
-                    titleStyle={{ color: theme.colors.white }}
-                />
-                <Divider />
-                <Menu.Item
-                    onPress={() => {
-                        setOrderBy('RATING_AVERAGE');
-                        setOrderDirection('ASC');
-                        closeMenu();
-                    }}
-                    title='Lowest rated'
-                    titleStyle={{ color: theme.colors.white }}
-                />
-                <Divider />
-                <Menu.Item
-                    onPress={() => {
-                        setOrderBy('RATING_AVERAGE');
-                        setOrderDirection('DESC');
-                        closeMenu();
-                    }}
-                    title='Highest rated'
-                    titleStyle={{ color: theme.colors.white }}
-                />
-                <Divider />
-                <Menu.Item
-                    onPress={() => {
-                        setOrderBy('CREATED_AT');
-                        setOrderDirection('DESC');
-                        closeMenu();
-                    }}
-                    title='Latest repositories'
-                    titleStyle={{ color: theme.colors.white }}
-                />
+                <Portal>
+                    <Modal
+                        visible={visible}
+                        onDismiss={closeMenu}
+                        contentContainerStyle={{
+                            backgroundColor: theme.colors.primary,
+                            alignItems: 'flex-end',
+                            flexWrap: 'wrap-reverse',
+                            alignSelf: 'center',
+                            justifyContent: 'flex-start',
+                        }}
+                        style={{
+                            marginTop: 0,
+                            padding: 2,
+                        }}
+                    >
+                        <Menu.Item
+                            onPress={() => {
+                                setOrderBy('RATING_AVERAGE');
+                                setOrderDirection('DESC');
+                                closeMenu();
+                            }}
+                            title='Select an item...'
+                            titleStyle={{
+                                color: theme.colors.white,
+                            }}
+                            contentStyle={{
+                                width: '100%',
+                                opacity: 0.4,
+                            }}
+                            theme={{
+                                colors: {
+                                    primary: theme.colors.white,
+                                    onSurfaceVariant: theme.colors.white,
+                                },
+                            }}
+                            disabled
+                        />
+                        <Divider
+                            style={{
+                                width: '100%',
+                            }}
+                        />
+                        <Menu.Item
+                            onPress={() => {
+                                setOrderBy('RATING_AVERAGE');
+                                setOrderDirection('DESC');
+                                closeMenu();
+                            }}
+                            title='Highest rated'
+                            titleStyle={{ color: theme.colors.white }}
+                            leadingIcon={{
+                                source: 'arrow-up',
+                                direction: 'ltr',
+                            }}
+                            contentStyle={{
+                                width: '100%',
+                            }}
+                            theme={{
+                                colors: {
+                                    primary: theme.colors.white,
+                                    onSurfaceVariant: theme.colors.white,
+                                },
+                            }}
+                        />
+                        <Divider
+                            style={{
+                                width: '100%',
+                            }}
+                        />
+                        <Menu.Item
+                            onPress={() => {
+                                setOrderBy('RATING_AVERAGE');
+                                setOrderDirection('ASC');
+                                closeMenu();
+                            }}
+                            title='Lowest rated'
+                            titleStyle={{ color: theme.colors.white }}
+                            contentStyle={{
+                                width: '100%',
+                            }}
+                            leadingIcon='arrow-down'
+                            theme={{
+                                colors: {
+                                    primary: theme.colors.white,
+                                    onSurfaceVariant: theme.colors.white,
+                                },
+                            }}
+                        />
+                        <Divider
+                            style={{
+                                width: '100%',
+                            }}
+                        />
+                        <Menu.Item
+                            onPress={() => {
+                                setOrderBy('CREATED_AT');
+                                setOrderDirection('DESC');
+                                closeMenu();
+                            }}
+                            title='Latest repositories'
+                            titleStyle={{ color: theme.colors.white }}
+                            contentStyle={{
+                                width: '100%',
+                            }}
+                            leadingIcon='autorenew'
+                            theme={{
+                                colors: {
+                                    primary: theme.colors.white,
+                                    onSurfaceVariant: theme.colors.white,
+                                },
+                            }}
+                        />
+                        <Divider
+                            style={{
+                                width: '100%',
+                            }}
+                        />
+                        <Menu.Item
+                            onPress={() => {
+                                setOrderBy('CREATED_AT');
+                                setOrderDirection('ASC');
+                                closeMenu();
+                            }}
+                            title='Oldest repositories'
+                            titleStyle={{ color: theme.colors.white }}
+                            contentStyle={{
+                                width: '100%',
+                            }}
+                            leadingIcon='timer-sand-complete'
+                            theme={{
+                                colors: {
+                                    primary: theme.colors.white,
+                                    onSurfaceVariant: theme.colors.white,
+                                },
+                            }}
+                        />
+                    </Modal>
+                </Portal>
             </Menu>
             <RepositoryListContainer repositories={repositories} />
         </View>
